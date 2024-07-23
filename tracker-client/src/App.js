e711711ff4eef058734a9ed4d47d07d0c92a875d
Hee-Ho <hang_jackyly@live.ca>
2024-07-22 18:59:01 -0600

    Add frontend structure for ui, fix sql bugs

--- tracker-client/src/App.js
+++ tracker-client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 // Routing mimics MPA (react is SPA)
 import React from 'react';
 import {
-  BrowserRouter,
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
@@ -13,6 +12,8 @@ import {
 import Home from "./pages/home"
 import Login from "./pages/login"
 import Signup from './pages/signup';
+import ProfilePage from './pages/ProfilePage/profilePage';
+import MangaPage from './pages/MangaPage/mangaPage';
 
 
 const routes = createBrowserRouter([
@@ -27,18 +28,32 @@ const routes = createBrowserRouter([
   {
     path: "signup",
     element: <Signup />
+  },
+  {
+    path: "manga",
+    element: <MangaPage/>
+  },
+  {
+    path: "profile/:user_id",
+    element: <ProfilePage/> 
   }
 ]
-
 )
-
-
 
 function App() {
   return (
     // Strict mode highlights possible problems. Does not create any visible elements
+    // navigation will place in header
     <React.StrictMode>
-      <RouterProvider router={routes} />
+      <div className='App'>
+        
+        <header className='web-header'> 
+          <h4> Header: Navigation go here</h4>
+        </header>
+        <div className='page-wrapper'> 
+          <RouterProvider router={routes} />
+        </div>
+      </div>
     </React.StrictMode>
   );
 }
